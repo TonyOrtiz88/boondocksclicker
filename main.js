0d6f7d5ae41be2f2275083c504f97551a9a6b1b4
Anthony Ortiz-Morris <anthony.ortiz.morris88@gmail.com>
2019-04-19 15:21:06 -0400

    almost_done

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+// button click for photo 2
 let i = 0;
     function buttonClick() {
         document.getElementById('inc').value = ++i;
     }
+// button click for photo 2 
+    let t = 0;
+    function buttonClick2() {
+        document.getElementById('link').value = ++t;
+    }
+
+    let  myObject = {
+        name:  'Thugnifecent n\' Sway' 
+        
+      };
+
+      
